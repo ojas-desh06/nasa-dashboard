669296d8e7e120daf18f77e241023c169f9c5150
ovd-study <ovd.study@gmail.com>
2026-02-09 18:44:52 -0500

    Use envioronment variable for API URL

--- frontend/nasa-frontend/src/services/api.js
+++ frontend/nasa-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 async function getAPOD() {
     const response  = await axios.get(`${API_BASE}/apod`);
